80a6212919159c443b6ca4007b714dbd16779b61
hamdiwael <wael.hamdi.m8@gmail.com>
2020-04-16 18:45:28 +0100

    my first commit

---
+++ src/tn/esprit/binder/Iservices/club/IActivityService.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tn.esprit.binder.Iservices.club;
+
+import tn.esprit.binder.entities.clubs.activity;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author Sam
+ */
+public interface IActivityService {
+    public void addActivity(activity a) throws SQLException;
+      public void deleteActivity(int id);
+        public boolean updateActivity(activity c) throws SQLException;
+         public List<activity> getAll();
+        public List<activity> rechercheActivity(String str);
+      public void deleteActivitymodified(int id);
+        public void addActivitymodify(activity c) throws SQLException;
+        public List<activity> rechercheActivityClub(String str);
+}
